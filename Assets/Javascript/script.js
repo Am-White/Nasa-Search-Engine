4916cf66dff64f0915bd296f75d8913ccb446917
SCN <supercodingninja@outlook.com>
2020-11-14 16:41:55 -0800

    List of API's to use

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -15,3 +15,9 @@ var search = "nebula";
             console.log(response.thumbnail.source);
             console.log(response.description);
         });
+
+// API's to use: 
+//     1. https://en.wikipedia.org/api/rest_v1/page/summary/pluto; 
+//     2. https://images-api.nasa.gov/search?q=sun
+//     3. https://api.le-systeme-solaire.net/en/
+// Use these to make results from API's show in "Result Photos" section
