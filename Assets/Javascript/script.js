5165a7f536a19c6aca875c14924aa2ca5ad77b4b
SCN <supercodingninja@outlook.com>
2020-11-16 20:50:58 -0800

    Still working on

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,9 +1,6 @@
 'use strict';
-// jQuery //
-$(document).ready(function(){
-  var search = $("btn btn-outline-success my-2 my-sm-0").val;
 
-});
+var search = "Jupiter";
 
 // Relaxed AJAX //
 function nasaImageSearch(search) {
@@ -12,25 +9,31 @@ function nasaImageSearch(search) {
     url: "https://images-api.nasa.gov/search?q=" + search + "&appid=", // DO I NEED API KEY FOR NASA??? //
     dataType: "JSON",
 
-    success: function(data) {
-      // I NEED HELP GETTING THIS TO GO INTO THE DIFFERENT </div>. We need to change where results populate (only one per area).  Maybe we need to use a ".then" function. //
-      $("#search").html("<div class="container-fluid mt-3">").append("<div class="row">");
-      
-        if (data.list[i].dt_img) {
+    // success: function (data) {
+    //   console.log (data);
+    //   I NEED HELP GETTING THIS TO GO INTO THE DIFFERENT </div>. We need to change where results populate (only one per area).  Maybe we need to use a ".then" function. //
+    //   $("#search").html("<div class='container-fluid mt-3'>").append("<div class='row'>");
 
-          // Create HTML elements. // 
-          var col = $("<div>").addClass("col-11 bg-success container-primary text-white");
-          var body = $("<div>").addClass("col-sm bg-warning ml-1 mb-1 d-flex align-items-end  flex-row" style="height:200px");
-          var title = $("<div>").addClass("align-text-bottom").text(new Date(data.list[i].dt_txt).toLocaleDateString());
-          // NO, NO, NO! var img = $("<img>").attr("src", "https://images-api.nasa.gov/search?q=" + data.list[i]. + ".png");
-          
+    //     if (data.list[i].dt_img) {
+
+    //       // Create HTML elements. // 
+    //       var col = $("<div>").addClass("col-11 bg-success container-primary text-white");
+    //       var body = $("<div>").addClass("col-sm bg-warning ml-1 mb-1 d-flex align-items-end  flex-row" style="height:200px");
+    //       var title = $("<div>").addClass("align-text-bottom").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+    //       // NO, NO, NO! var img = $("<img>").attr("src", "https://images-api.nasa.gov/search?q=" + data.list[i]. + ".png");
+
           // I need to merge all this together... //
           col.append(card.append(body.append(title)));
           $("#search").append(col);
-    
-      }
+
     }
-  });.then(function (response) {
-    console.log(response);
+
+  });
 
 }
+
+// Event Listener //
+$(document).ready(function () {
+  // var search = $("btn btn-outline-success my-2 my-sm-0").val;
+  nasaImageSearch;
+});
