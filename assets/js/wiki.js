461216a40d8f1eb988f5db4a3e861eeee761ae17
SCN <supercodingninja@outlook.com>
2020-11-19 01:09:18 -0800

    Wiki.js update.

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $("#send").on("click", function(event) {
             event.preventDefault();
 
@@ -34,46 +33,3 @@ $("#send").on("click", function(event) {
             // $(".card-footer").text(response2.date);
             $('#pic-od').find('.card-image').attr("onclick","window.open('"+response2.url+"')");
         });
-=======
-
-function wikiContent(searchWord){
-   
-            
-    var inputEl = searchWord;
-    var wikiURL = "https://en.wikipedia.org/api/rest_v1/page/summary/" + inputEl;
-
-    $.ajax({
-        url: wikiURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log("wiki");
-        console.log(response);
- 
-        $('#projectname').text(response.displaytitle);
-        $('#projectname').next().text(response.extract);
-    });
-
-}
-var today = moment().format("YYYY-MM-DD");
-
-var key="puubY5HJoKpmDOhcrRYD9nZIxJ2we1gX9lXjQoRl";
-
-var dayImg = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date=" + today;
-
-$.ajax({
-    url: dayImg,
-    method: "GET"
-}).then(function(response2) {
-    console.log(response2);
-   
-    $('#pic-od').find('.card-image').attr("style","background-image: url("+response2.url+")");
- 
-    $('#pic-od').find('.card-image').attr("onclick","window.open('"+response2.url+"')");
-
-    $('#pic-od').find('.tile__container').text(response2.title);
-    $('#pic-od').find('.tile__container').parent().next().text(response2.explanation);
-
-    $(".card-footer").text(response2.date);
-
-});
->>>>>>> 84d5ec94e033fe50272d10a0c5407fad5647270b
