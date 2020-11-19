928c9ef5869711b4e3267b758d342c545fdd605f
SCN <supercodingninja@outlook.com>
2020-11-18 20:14:01 -0800

    ...

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -9,18 +9,18 @@ function nasaImageSearch(search) {
     url: "https://images-api.nasa.gov/search?q=" + search + "&appid=", // DO I NEED API KEY FOR NASA??? //
     dataType: "JSON",
 
-    // success: function (data) {
-    //   console.log (data);
-    //   I NEED HELP GETTING THIS TO GO INTO THE DIFFERENT </div>. We need to change where results populate (only one per area).  Maybe we need to use a ".then" function. //
-    //   $("#search").html("<div class='container-fluid mt-3'>").append("<div class='row'>");
+    success: function (data) {
+      console.log (data);
+      I NEED HELP GETTING THIS TO GO INTO THE DIFFERENT </div>. We need to change where results populate (only one per area).  Maybe we need to use a ".then" function. //
+      $("#search").html("<div class='container-fluid mt-3'>").append("<div class='row'>");
 
-    //     if (data.list[i].dt_img) {
+        if (data.list[i].dt_img) {
 
-    //       // Create HTML elements. // 
-    //       var col = $("<div>").addClass("col-11 bg-success container-primary text-white");
-    //       var body = $("<div>").addClass("col-sm bg-warning ml-1 mb-1 d-flex align-items-end  flex-row" style="height:200px");
-    //       var title = $("<div>").addClass("align-text-bottom").text(new Date(data.list[i].dt_txt).toLocaleDateString());
-    //       // NO, NO, NO! var img = $("<img>").attr("src", "https://images-api.nasa.gov/search?q=" + data.list[i]. + ".png");
+          // Create HTML elements. // 
+          var col = $("<div>").addClass("col-11 bg-success container-primary text-white");
+          var body = $("<div>").addClass("col-sm bg-warning ml-1 mb-1 d-flex align-items-end  flex-row" style="height:200px");
+          var title = $("<div>").addClass("align-text-bottom").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+          // NO, NO, NO! var img = $("<img>").attr("src", "https://images-api.nasa.gov/search?q=" + data.list[i]. + ".png");
 
           // I need to merge all this together... //
           col.append(card.append(body.append(title)));
